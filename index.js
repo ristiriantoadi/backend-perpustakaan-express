d49cfa952a869d4f03ecec9b8c3487761d13bf57
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-01-30 06:01:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
 const cors = require('cors')
 app.use(cors())
-
 app.use(express.json())
-
 require('dotenv').config();
 
 //jwt
