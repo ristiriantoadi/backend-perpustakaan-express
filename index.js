9e45466109dce7a62b3aab98269c5fa40877ed6e
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-02-13 11:20:29 +0800

    change process env port to process env PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path');
 require('dotenv').config();
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
